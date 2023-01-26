2251a6f5446bfcd86c9d12eecedc3e35cbbcbeb2
josh137xd <112659525+josh137xd@users.noreply.github.com>
2023-01-26 16:20:51 -0500

    Delete functionality implemented

--- controllers/places.js
+++ controllers/places.js
@@ -11,7 +11,7 @@ router.get('/new', (req, res) => {
     res.render('places/new')
   })
 
-  router.get('/:id', (req, res) => {
+router.get('/:id', (req, res) => {
     let id = Number(req.params.id)
     if (isNaN(id)) {
       res.render('error404')
@@ -20,8 +20,20 @@ router.get('/new', (req, res) => {
       res.render('error404')
     }
     else {
-        res.render('places/show', { place: places[id] })
-
+      res.render('places/show', { place: places[id], id })
+    }
+  })
+router.delete('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+      res.render('error404')
+    }
+    else if (!places[id]) {
+      res.render('error404')
+    }
+    else {
+      places.splice(id, 1)
+      res.redirect('/places')
     }
   })
   
--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-//modules and globals
+// Modules and Globals
 require('dotenv').config()
 const express = require('express')
+const methodOverride = require('method-override')
 const app = express()
 app.use(express.urlencoded({ extended: true }))
 
-// express settings
+// Express Settings
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 //controllers & routes
 app.use('/places', require('./controllers/places'))
