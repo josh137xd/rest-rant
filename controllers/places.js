a9eef5cb4bea459b49143ad2aa436243591ad32f
josh137xd <112659525+josh137xd@users.noreply.github.com>
2023-01-26 15:54:22 -0500

    Show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -10,8 +10,22 @@ router.get('/', (req, res) => {
 router.get('/new', (req, res) => {
     res.render('places/new')
   })
- 
-  router.post('/', (req, res) => {
+
+  router.get('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+      res.render('error404')
+    }
+    else if (!places[id]) {
+      res.render('error404')
+    }
+    else {
+        res.render('places/show', { place: places[id] })
+
+    }
+  })
+  
+router.post('/', (req, res) => {
     if (!req.body.pic) {
       // Default image if one is not provided
       req.body.pic = 'http://placekitten.com/400/400'
