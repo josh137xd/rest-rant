d8ad90de1e23b0cd92759ba7e560a4729aa5e9e6
josh137xd <112659525+josh137xd@users.noreply.github.com>
2023-01-26 15:49:53 -0500

    Navbar with style

--- views/default.jsx
+++ views/default.jsx
@@ -7,9 +7,23 @@ function Def (html) {
                 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" cross0rigin="anonymous"/>
                 <link rel="stylesheet" href="style.css"/>
             </head>
-            <body>
-                {html.children}
-            </body>
+<body>
+    <nav>
+      <ul>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/places">Places</a>
+        </li>
+        <li>
+          <a href="/places/new">Add Place</a>
+        </li>
+      </ul>
+    </nav>
+
+{html.children}
+</body>
         </html>
     )
   }
