57dea7227d487b0dff46904fd4636f21b30af52a
josh137xd <112659525+josh137xd@users.noreply.github.com>
2023-01-26 14:44:17 -0500

    New places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,30 +1,30 @@
 const router = require('express').Router()
+const places = require('../models/places.js')
 
-// More code here in a moment
+router.get('/', (req, res) => {
+    res.render('places/index', { places })
+})
+
+// More code ...
+
 router.get('/new', (req, res) => {
     res.render('places/new')
   })
+ 
   router.post('/', (req, res) => {
-   console.log (req, res)
-    res.send('POST /places')
-  });
-router.get('/', (req, res) => {
-    let places = [{
-     name: 'high-end',
-     city: 'Seattle',
-     state: 'WA',
-     cuisines: 'argetinan, american',
-     pic: '/images/steak-house.jpg'
-    }, 
-     {
-        name: 'eifel Cafe',
-     city: 'Phoenix',
-     state: 'AZ',
-     cuisines: 'Coffee, french bakery',
-     pic: '/images/french-caffe.jpg'
-     }]
-
- res.render('places/index', { places })
-})
+    if (!req.body.pic) {
+      // Default image if one is not provided
+      req.body.pic = 'http://placekitten.com/400/400'
+    }
+    if (!req.body.city) {
+      req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+      req.body.state = 'USA'
+    }
+    places.push(req.body)
+    res.redirect('/places')
+  })
+  
 
 module.exports = router
---
+++ models/places.js
@@ -0,0 +1,14 @@
+module.exports = [{
+    name: 'high-end',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'argetinan, american',
+    pic: '/images/steak-house.jpg'
+   }, 
+    {
+       name: 'eifel Cafe',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Coffee, french bakery',
+    pic: '/images/french-caffe.jpg'
+    }]
